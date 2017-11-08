f913c33d94a2ee3b7f5992c7396a5a8c58945003
ykomuro0719 <y_komuro@r.recruit.co.jp>
2017-11-08 13:39:43 +0900

    [update]Drink回数カウント機能追加

--- app/controllers/drink_counts_controller.rb
+++ app/controllers/drink_counts_controller.rb
@@ -2,8 +2,17 @@ class DrinkCountsController < ApplicationController
   def index
     @users = User.all
     @drinks = Drink.all
+    @drinkcounts = DrinkCount.where(updated_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
   end
   def create
+    
+
+    @drinkcount = DrinkCount.create(user_id: params[:user_id], drink_id: params[:id], cnt: "true")
+    @drinkcount.save
   redirect_to :root
+end
+private
+  def drink_count_params
+      params.require(:drink_count).permit(:user_id, :drink_id, :cnt, :id)
   end
 end
