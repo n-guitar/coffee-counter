5c4e508d1a98e4e7a830de9f1b33f9474fca4a55
ykomuro0719 <y_komuro@r.recruit.co.jp>
2017-11-09 21:12:25 +0900

    [update] カウントAPI作成

--- app/controllers/drink_counts_controller.rb
+++ app/controllers/drink_counts_controller.rb
@@ -2,17 +2,19 @@ class DrinkCountsController < ApplicationController
   def index
     @users = User.all
     @drinks = Drink.all
-    @drinkcounts = DrinkCount.where(updated_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
+    @drinkcounts = DrinkCount.all#.where(updated_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
+    @drinkcount = DrinkCount.new
+ 
   end
   def create
     
-
-    @drinkcount = DrinkCount.create(user_id: params[:user_id], drink_id: params[:id], cnt: "true")
+    user = User.find_by(:name => params[:commit])
+    @drinkcount = DrinkCount.create(user_id: user.id, drink_id: params[:drink_id], cnt: "true")
     @drinkcount.save
   redirect_to :root
 end
 private
   def drink_count_params
-      params.require(:drink_count).permit(:user_id, :drink_id, :cnt, :id)
+      params.require(:drink_count).permit(:user_id, :drink_id, :cnt)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Counter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.active_record.default_timezone = "Tokyo"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
