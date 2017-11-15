452af775e8f8292f0b594c11d43b197a317c2c61
ykomuro0719 <y_komuro@r.recruit.co.jp>
2017-11-15 13:12:18 +0900

    [update]Createメソッド更新

--- app/controllers/drink_counts_controller.rb
+++ app/controllers/drink_counts_controller.rb
@@ -2,19 +2,16 @@ class DrinkCountsController < ApplicationController
   def index
     @users = User.all
     @drinks = Drink.all
-    @drinkcounts = DrinkCount.all#.where(updated_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
+    @drinkcounts = DrinkCount.where(updated_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
     @drinkcount = DrinkCount.new
  
   end
-  def create
-    
+  
+  def create   
     user = User.find_by(:name => params[:commit])
-    @drinkcount = DrinkCount.create(user_id: user.id, drink_id: params[:drink_id], cnt: "true")
+    @drinkcount = DrinkCount.create(user_id: user.id, cnt: true, drink_id: params[:drink_count][:drink_id] )
     @drinkcount.save
   redirect_to :root
-end
-private
-  def drink_count_params
-      params.require(:drink_count).permit(:user_id, :drink_id, :cnt)
   end
+
 end
