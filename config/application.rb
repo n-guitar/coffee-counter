61929224a3c8553177ad287f60e60beb5fa93328
Komuro Yoshitaka <xiaoshieconomics@hotmail.co.jp>
2017-11-18 03:47:10 +0000

    [update]IndexのViewを変更

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Counter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.active_record.default_timezone = "Tokyo"
+    #config.active_record.default_timezone = "Tokyo"
     config.action_controller.permit_all_parameters = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
