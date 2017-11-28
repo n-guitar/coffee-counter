6b7f3a09c386a958481d4e76e6cb7d9c91260080
小室良貴 <yoshitaka@yk-mb.local>
2017-11-29 08:07:49 +0900

    [update]Dashboard更新

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,8 +5,12 @@ class DashboardsController < ApplicationController
     @drinkcounts = DrinkCount.where(updated_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
     @drinkranks = @drinkcounts.group(:drink_id).order('count_all desc').count
 
-    gon.drinkranks_name = @drinkranks.keys.map{|m| Drink.find(m).menu}
-    gon.drinkranks_count = @drinkranks.values
+    keys = @drinkranks.keys.map{|m| Drink.find(m).menu}
+    values = @drinkranks.values
+    ary = [keys,values].transpose
+    gon.drinkranks_name = keys
+    gon.drinkranks_count = values
+    gon.drinkranks = Hash[*ary.flatten]
 
   end
 end
--- app/controllers/drink_counts_controller.rb
+++ app/controllers/drink_counts_controller.rb
@@ -8,15 +8,6 @@ class DrinkCountsController < ApplicationController
 
   end
 
-  def index2
-    @users = User.where(disabled: false).order(:name)
-    @drinks = Drink.all
-    @drinkcounts = DrinkCount.where(updated_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
-    @drinkcount = DrinkCount.new
-
-  end
-
-
   def create
     user = User.find_by(:name => params[:commit])
     @drinkcount = DrinkCount.create(user_id: user.id, cnt: true, drink_id: params[:drink_count][:drink_id] )
